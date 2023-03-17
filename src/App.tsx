12035c887684f72cb7b89f2eda40d7f6c2eee3df
Haleigh Twohy <haleightwohy@Haleighs-MBP.lan>
2023-03-17 12:01:39 -0400

    showing and hiding alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVistable, setAlertVisibility] =useState(false);
 
   return (
     <div>
-      <Alert>
+      { alertVistable && <Alert onClose={() => setAlertVisibility(false)}>
         Hello <div>World</div>
-      </Alert>
-      <Button onClick={() => console.log('clicked')}>My Button</Button>
+      </Alert>}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
