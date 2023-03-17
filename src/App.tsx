094f8db615f2ae32d450621e8250037f23dbbf69
Haleigh Twohy <haleightwohy@Haleighs-MBP.lan>
2023-03-17 11:43:16 -0400

    building button component exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
       <Alert>
         Hello <div>World</div>
       </Alert>
+      <Button onClick={() => console.log('clicked')}>My Button</Button>
     </div>
   );
 }
