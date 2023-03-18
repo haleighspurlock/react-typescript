4ea001455c5de4533c8d24cfe5d1fd3012eda810
Haleigh Twohy <haleightwohy@Haleighs-MBP.lan>
2023-03-18 09:50:35 -0400

    choosing state structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
 
 function App() {
-  const [alertVistable, setAlertVisibility] =useState(false);
+  const [person, setPerson] = useState({
+    firstName: '',
+    lastName: '',
+    contact: {
+      address: {
+        street: ''
+      }
+    }
+  });
+  const [isLoading, setLoading] = useState(false);
 
   return (
     <div>
-      { alertVistable && <Alert onClose={() => setAlertVisibility(false)}>
-        Hello <div>World</div>
-      </Alert>}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+
     </div>
   );
 }
