b039ea7658c564d76567125d5c5f96adfb80e0a2
Haleigh Twohy <haleightwohy@Haleighs-MBP.lan>
2023-03-18 12:00:46 -0400

    updating objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
-import Message from "./Message";
+// import Message from "./Message";
+
 function App() {
+  const [drink, setDrink] = useState ({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    setDrink({...drink, price:6})
+  }
   return (
     <div>
-      <Message />
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
