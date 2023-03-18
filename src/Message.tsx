29c822463419cf458d8f808e801c36e2f58bfc88
Haleigh Twohy <haleightwohy@Haleighs-MBP.lan>
2023-03-18 09:54:24 -0400

    keeping components pure

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,10 +1,7 @@
-//PascalCasing
-function Message() {
-    //JSX: JavaScript XML
-    const name = '';
-    if (name)
-    return <h1>Hello {name}</h1>;
-    return <h1>Hello World</h1>
-}
+const Message = () => {
+    let count = 0;
+    count++;
+    return <div> Message {count} </div>
+};
 
 export default Message;
