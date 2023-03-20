bfcea64026a7a9084b467c77a00a4929f91a9afb
Haleigh Twohy <haleightwohy@Haleighs-MBP.lan>
2023-03-20 09:18:58 -0400

    update state exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
 // import Message from "./Message";
-import produce from 'immer';
 
 function App() {
-  const [bugs, setBugs] = useState ([
-    {id: 1, title: 'bug 1', fixed: false},
-    {id: 2, title: 'bug 2', fixed: false},
-  ]);
+  const [game, setGame] = useState ({
+    id: 1,
+    player: {
+      name: 'Oliver',
+    }
+  })
 
   const handleClick = () => {
-    //setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true} : bug))
-    setBugs(produce(draft => {
-      const bug = draft.find(bug => bug.id === 1)
-      if (bug) bug.fixed = true; 
-    }))
+    setGame({...game, player: {...game.player, name: 'Twohy'} })
   }
+  
   return (
     <div>
       <button onClick={handleClick}>Click Me</button>
