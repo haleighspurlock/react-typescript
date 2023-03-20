1dc71d02c9d6f3628f3c3eb736942f594637ef27
Haleigh Twohy <haleightwohy@Haleighs-MBP.lan>
2023-03-20 09:29:14 -0400

    update state exercise 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 // import Message from "./Message";
 
 function App() {
-  const [pizza, setPizza] = useState ({
-    name: 'Spicy Pepperoni',
-    toppings: ['Mushrooms']
+  const [cart, setCart] = useState ({
+    discount: .1,
+    items: [
+      { id: 1, title: 'product 1', quantity: 1 },
+      { id: 2, title: 'product 2', quantity: 1}
+    ]
   })
 
   const handleClick = () => {
-    setPizza({...pizza, toppings: [...pizza.toppings, 'Cheese'] })
+    setCart({...cart, items: [...cart.items.map(item => item.id === 1 ? { ...item, quantity:2 } : item )] })
   }
 
   return (
