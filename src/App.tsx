13fe0df0d9f0cc27b46dfeca13c704d4d5ef7bb5
Haleigh Twohy <haleightwohy@Haleighs-MBP.lan>
2023-03-21 09:44:45 -0400

    building a form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
 // import Message from "./Message";
+import Form from "./components/Form"
 
 function App() {
-  const [cart, setCart] = useState ({
-    discount: .1,
-    items: [
-      { id: 1, title: 'product 1', quantity: 1 },
-      { id: 2, title: 'product 2', quantity: 1}
-    ]
-  })
+  // const [cart, setCart] = useState ({
+  //   discount: .1,
+  //   items: [
+  //     { id: 1, title: 'product 1', quantity: 1 },
+  //     { id: 2, title: 'product 2', quantity: 1}
+  //   ]
+  // })
 
-  const handleClick = () => {
-    setCart({...cart, items: [...cart.items.map(item => item.id === 1 ? { ...item, quantity:2 } : item )] })
-  }
+  // const handleClick = () => {
+  //   setCart({...cart, items: [...cart.items.map(item => item.id === 1 ? { ...item, quantity:2 } : item )] })
+  // }
 
   return (
-    <div>
-      <button onClick={handleClick}>Click Me</button>
-    </div>
+    // <div>
+    //   <button onClick={handleClick}>Click Me</button>
+    // </div>
+    <Form />
   );
 }
 
